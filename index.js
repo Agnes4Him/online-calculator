a3defdc705573b9d2d60cf54ba5a65336279cc80
Agnes4Him <ojuhagnes@gmail.com>
2022-07-14 09:57:31 +0100

    Added extra functionality to avoid inputing consecutive operators

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const operation = document.querySelectorAll('.numop')
+const numOp = document.querySelectorAll('.numop')
+const plusOp = document.getElementById('plusOp')
+const minusOp = document.getElementById('minusOp')
+const timesOp = document.getElementById('timesOp')
+const divideOp = document.getElementById('divideOp')
+const dotOp = document.getElementById('dotOp')
+const number = document.querySelectorAll('.number')
 const display = document.getElementById('display')
 const equal = document.querySelector('.equal')
 const clear = document.querySelector('.clear')
@@ -6,14 +12,22 @@ const on = document.querySelector('.on')
 const off = document.querySelector('.off')
 const arrow = document.querySelector('.arrow')
 
-operation.forEach((eachOperation) => {
-    eachOperation.addEventListener('click', () => {
-        if (display.innerHTML == 0) {
-            display.innerHTML = ""
-            display.innerHTML += eachOperation.innerHTML
+numOp.forEach((eachnumOp) => {
+    eachnumOp.addEventListener('click', () => {
+        if ((display.innerHTML).length > 0) {
+            const lastItem = (display.innerHTML).charAt((display.innerHTML).length - 1)
+            if ((eachnumOp.innerHTML == plusOp.innerHTML || eachnumOp.innerHTML == minusOp.innerHTML || eachnumOp.innerHTML == timesOp.innerHTML || eachnumOp.innerHTML == divideOp.innerHTML || eachnumOp.innerHTML == dotOp.innerHTML) && (lastItem == plusOp.innerHTML || lastItem == minusOp.innerHTML || lastItem == timesOp.innerHTML || lastItem == divideOp.innerHTML || lastItem == dotOp.innerHTML)) {
+                display.innerHTML = (display.innerHTML).slice(0, -1)
+                display.innerHTML += eachnumOp.innerHTML
+            }else if (display.innerHTML == "0") {
+                display.innerHTML = ""
+                display.innerHTML += eachnumOp.innerHTML
+            }else {
+                display.innerHTML += eachnumOp.innerHTML
+            }
         }else {
-            display.innerHTML += eachOperation.innerHTML
-        }
+            display.innerHTML += eachnumOp.innerHTML
+        }    
     })
 })
 
@@ -36,6 +50,3 @@ off.addEventListener('click', () => {
 arrow.addEventListener('click', () => {
     display.innerHTML = (display.innerHTML).slice(0, (display.innerHTML).length - 1)
 })
-
-
-
